fa46620f45198ae63f17394d5263e5dec034c53e
AP <ap@a-pfau.de>
2022-05-25 16:55:33 +0200

    v2

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Typewriter from "typewriter-effect/dist/core";
-import "./assets/css/main.css";
-import "./assets/css/themes.dark.css";
-
-new Typewriter("#typewriter", {
-  strings: ["Planing", "Developing", "Deployment"],
-  autoStart: true,
-  loop: false,
-});
